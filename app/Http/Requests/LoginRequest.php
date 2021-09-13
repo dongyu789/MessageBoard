ea28855eabbe61c489086c6581e04319b505c581
dongyu789 <1207155125@qq.com>
2021-09-13 16:10:07 +0800

    x

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -25,8 +25,8 @@ class LoginRequest extends FormRequest
     {
         return [
             //不得为空，用户和密码的长度都在6-8个字符,用户和密码只能填写字母数字下划线
-            'username' => 'required | min:6 | max:8 | alpha_dash',
-            'pwd' => 'required | min:6 | max:8 | alpha_dash'
+            'username' => 'required | min:6 | max:20 | alpha_dash | exists:users,user_id',
+            'pwd' => 'required | min:6 | max:20 | alpha_dash'
         ];
 
     }
@@ -34,13 +34,14 @@ class LoginRequest extends FormRequest
     public function messages()
     {
         return [
+            'username.exists' => "用户名不存在或已注销",
             'username.required'=> "用户名不得为空",
             'username.min' => '用户名不得小于6个字符',
-            'username.max' => '用户名不得大于8个字符',
+            'username.max' => '用户名不得大于20个字符',
             'username.alpha_dash' => '用户名只能写字母数字下划线',
             'pwd.required'=> "密码不得为空",
             'pwd.min' => '密码不得小于6个字符',
-            'pwd.max' => '密码不得大于8个字符',
+            'pwd.max' => '密码不得大于20个字符',
             'pwd.alpha_dash' => '密码只能写字母数字下划线',
         ];
     }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,9 +24,9 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            //不得为空，用户和密码的长度都在6-8个字符,用户和密码只能填写字母数字下划线
-            'username' => 'required | min:6 | max:8 | alpha_dash',
-            'pwd' => 'required | min:6 | max:8 | alpha_dash | confirmed'
+            //不得为空，用户和密码的长度都在6-20个字符,用户和密码只能填写字母数字下划线
+            'username' => 'required | min:6 | max:20 | alpha_dash',
+            'pwd' => 'required | min:6 | max:20 | alpha_dash | confirmed'
         ];
 
     }
@@ -36,11 +36,11 @@ class RegisterRequest extends FormRequest
         return [
             'username.required'=> "用户名不得为空",
             'username.min' => '用户名不得小于6个字符',
-            'username.max' => '用户名不得大于8个字符',
+            'username.max' => '用户名不得大于20个字符',
             'username.alpha_dash' => '用户名只能写字母数字下划线',
             'pwd.required'=> "密码不得为空",
             'pwd.min' => '密码不得小于6个字符',
-            'pwd.max' => '密码不得大于8个字符',
+            'pwd.max' => '密码不得大于20个字符',
             'pwd.alpha_dash' => '密码只能写字母数字下划线',
             'pwd.confirmed' => '两次输入的密码不同'
         ];
